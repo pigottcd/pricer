9668583b9e557da13f08ad4bac2c4deda2d62cb7
Charlie Pigott <pigottcd@gmail.com>
2025-05-26 18:34:21 -0400

    project reordering, options, clang format/tidy changes

---
+++ bindings/python/bindings.cpp
@@ -0,0 +1,4 @@
+#include <pricer/sample_library.hpp>
+#include <pybind11/pybind11.h>
+
+PYBIND11_MODULE(pypricer, m) { m.def("factorial", &factorial, "A function that computes factorials"); }
--- src/python_bindings/bindings.cpp
+++
@@ -1,6 +0,0 @@
-#include <pricer/sample_library.hpp>
-#include <pybind11/pybind11.h>
-
-PYBIND11_MODULE(pypricer, m) {
-    m.def("factorial", &factorial, "A function that computes factorials");
-}
